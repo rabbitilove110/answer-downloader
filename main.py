c83160ecf336098f7362c1bbdbd294e0fa2e7b6f
Fruit <kyj110618@gmail.com>
2023-11-05 20:48:38 +0900

    error edit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ with open("./database.txt", "r", encoding='UTF-8') as file:
     for line in file:
         key, value, data = line.strip().split(',')
         data_dict[key] = value
+for key, value in data_dict.items():
+    data_dict[key] = value.strip()
 
 print("Answer sheet downloader, ver.1.0dev\n\n문제집 이름과 출판사를 적어주세요.\n예시 : OO수학 - OO문고\n\n주의사항 : github 사이트를 참고해주세요.\n")
 name = input("책이름을 입력해주세요 : ")
@@ -16,7 +18,7 @@ print("--------------------------------")
 print("잠시만 기다려주세요. DB에서 데이터를 분석하고있습니다.")
 time.sleep(1)
 
-if (name in data_dict and book in data_dict) or (name + '-1' in data_dict and book + '-1' in data_dict):
+if (name in data_dict.keys() and book in data_dict.values()) or (name + '-1' in data_dict.keys() and book + '-1' in data_dict.values()):
     data_key = name if name in data_dict else name + '-1'
     data_key2 = book if book in data_dict else book + '-1'
     data3 = data_dict[data_key]
