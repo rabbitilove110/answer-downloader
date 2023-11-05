261fd3693fb083fb46c1b93a568273f8cc9c4c7d
Fruit <kyj110618@gmail.com>
2023-11-05 09:17:34 +0900

    commit

--- main.py
+++ main.py
@@ -8,25 +8,27 @@ book = input("출판사를 입력해주세요 : ")
 
 print("--------------------------------")
 print("잠시만 기다려주세요. DB에서 데이터를 분석하고있습니다.")
-dbread = open("./database.txt", "r",encoding='UTF-8')
-data = dbread.read()
+data_dict = {}
+with open("database.txt", "r", encoding='UTF-8') as file:
+    for line in file:
+        key, value = line.strip().split(',')
+        data_dict[key] = value
+# dbread = open("./database.txt", "r",encoding='UTF-8')
+# data = dbread.read()
 time.sleep(3)
 
-database = True
-if database:
-    print("--------------------------------")
-    print(name + "이(가) 정상적으로 입력됬습니다. DB에서 데이터를 추출합니다.\n잠시만 기다려 주세요.")
-    time.sleep(5)
+if (name in data_dict and book in data_dict) or (name + '-1' in data_dict and book + '-1' in data_dict):
+    data_key = name if name in data_dict else name + '-1'
+    data_key2 = book if book in data_dict else book + '-1'
+    data3 = data_dict[data_key]
 else:
     print("--------------------------------")
     print("DB에서 책을 찾지 못했습니다. github Issues로 등록하면 다음 업데이트에 반영됩니다.\n10초뒤, 코드를 자동 종료합니다.")
-    dbread.close()
     time.sleep(10)
     exit()
 
-dataurl = (data)
+dataurl = (f"{data_key}:", data3)
 print("--------------------------------")
 print(dataurl + "을(를) 인터넷 주소창에 입력하시면 자동 다운로드가 진행됩니다.\n1분뒤, 코드를 자동 종료합니다.")
-dbread.close()
 time.sleep(60)
 exit()
